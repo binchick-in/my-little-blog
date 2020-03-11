f829ebaabb46144e571bae4801064d2bf6157cc3
binchick.in <moebob24@gmail.com>
2020-03-11 22:58:29 +0000

    Adding some contact info & removing some comments

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,10 @@ from app.error_views import general_error
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule('/', view_func=Main.as_view('home'))  # Home page of the blog
-    app.add_url_rule('/<string:page>', view_func=Page.as_view('page'))  # Page route
+    app.add_url_rule('/', view_func=Main.as_view('home'))
+    app.add_url_rule('/<string:page>', view_func=Page.as_view('page'))
     app.add_url_rule('/post/<string:post>', view_func=Post.as_view('post'))  
     app.add_url_rule('/posts', view_func=Posts.as_view('posts'))
     app.register_error_handler(404, general_error)
     return app
+
--- app/pages/contact.md
+++ app/pages/contact.md
@@ -3,5 +3,5 @@ title: Contact
 path: contact
 published: 2020-03-08
 ---
-
-Contact me!
+* [Github](https://github.com/binchick-in)
+* [Telegram](https://t.me/binchickin)
